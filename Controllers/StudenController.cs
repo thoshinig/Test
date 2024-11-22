2c7751ad8495b31517262ee293a2be0a0c56893b
thoshinig <thoshini612@gmail.com>
2024-11-22 19:00:53 +0530

    Add files via upload

---
+++ Controllers/StudenController.cs
@@ -0,0 +1,31 @@
+﻿using Evaluation.Models;
+using FirstProject.Models.Repository;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Evaluation.Controllers
+{
+    [Route("api/student")]
+    [ApiController]
+    public class StudenController : ControllerBase
+    {
+        private readonly IDataRepository<Student> _dataRepository;
+       
+        public StudenController(IDataRepository<Student> dataRepository)
+        {
+            _dataRepository=dataRepository;
+        }
+        //[HttpGet]
+        //public IActionResult GetAll()
+        //{
+        //    IEnumerable<Student> Student = _dataRepository.GetAll();
+        //    return Ok(Student);
+        //}
+        [HttpGet]
+        public async Task<IActionResult> Get(int id)
+        {
+            Student student = await _dataRepository.FiSamplefilters(id);
+            return Ok(student);
+        }
+    }
+}
