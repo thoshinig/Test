b1927e1cbe15b8259d28f65a01c6051711855f82
thoshinig <thoshini612@gmail.com>
2024-11-22 18:58:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Evaluation.Models;
+using Evaluation.Models.Data;
+using FirstProject.Models.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<StudentContext>(opts => opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDatabase")));
+builder.Services.AddScoped<IDataRepository<Student>, StudentData>();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
